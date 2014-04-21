3de66d32c63f2df4158a86d14bab9a04139dc3f8
swapnilkadam18 <kadam.swapnil09@gmail.com>
2014-04-21 16:06:44 +0530

    -added show log method

--- LiveVideoStreamer/src/com/rolta/dss/livevideostreamer/Constants.java
+++ LiveVideoStreamer/src/com/rolta/dss/livevideostreamer/Constants.java
@@ -2,9 +2,15 @@ package com.rolta.dss.livevideostreamer;
 
 import java.util.ArrayList;
 
+import android.util.Log;
+
 public class Constants {
 	//to maintain the sequence for fetching the data latter.
 	//in constants so scope will be global
 	public static ArrayList<String> videoSequence = new ArrayList<String>();
 
+	
+	public void showLog(String TAG, String message){
+		Log.e(TAG, message);
+	}
 }
